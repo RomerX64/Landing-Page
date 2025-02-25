00bf3c5b7c38312d7c968faf57c637a82b1c7c52
RomerX64 <tomiromera2903@gmail.com>
2025-02-25 15:23:34 -0300

    ...

---
+++ client/.next/static/chunks/app_(default)_profile_layout_tsx_d3d52f._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_(default)_profile_layout_tsx_d3d52f._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_(default)_profile_layout_tsx_d3d52f._.js",
+  "chunks": [
+    "static/chunks/node_modules_f0c63b._.js",
+    "static/chunks/_010e33._.js"
+  ],
+  "source": "dynamic"
+});
