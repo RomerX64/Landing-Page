c2dbde14ffa4aa754d370e65e6e632ea8c611309
RomerX64 <tomiromera2903@gmail.com>
2025-03-06 15:35:35 -0300

    ...

--- client/utils/Api.ts
+++ client/utils/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://assetly-landing-page-backend.onrender.com", //process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
