8e7d795041686425881e9cfb4c2dc9379ac2af72
RomerX64 <tomiromera2903@gmail.com>
2025-04-22 11:10:20 -0300

    year/month

--- client/components/tiposDePlanesSaaS.tsx
+++ client/components/tiposDePlanesSaaS.tsx
@@ -99,7 +99,7 @@ const CardPlan: React.FC<CardPlanProps> = ({
             {/* Precio */}
             <span className="inline-block min-w-[80px] rounded-full bg-gradient-to-r from-indigo-400 to-pink-400 px-3 py-1 text-sm font-bold text-white shadow-sm animate-gradient text-center">
               {precio > 0
-                ? precio > 10000
+                ? precio > 10000000
                   ? `$${precio}/y`
                   : `$${precio}/m`
                 : "Free"}
