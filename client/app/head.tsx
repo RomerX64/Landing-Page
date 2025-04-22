3cc46c44c2b14febce4ef6421621244c4f5a59d3
RomerX64 <tomiromera2903@gmail.com>
2025-04-22 10:51:14 -0300

    fixes

---
+++ client/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Assetly</title>
+      <meta
+        name="description"
+        content="Tu servicio para administrar tus activos"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
